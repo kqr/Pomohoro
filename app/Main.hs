61a9f295ed078775d582119394e1885a9eef312e
kqr <k@rdw.se>
2016-10-05 10:49:24 +0200

    add a status command

--- app/Main.hs
+++ app/Main.hs
@@ -15,6 +15,7 @@ import Networking
 
 data Command
     = Start { _account :: Text, _comment :: Text }
+    | Status
     | Interrupt
     | RemindMe { _minutes :: Int, _message :: Text }
     deriving Show
@@ -31,6 +32,11 @@ args =
             "Start a new work session for the specified timeclock",
             " account (or default if none specified)"
         ]))),
+        command "status" (info (
+            pure Status
+        ) (progDesc
+            "How long has the current session been going on for?"
+        )),
         command "int" (info (
             pure Interrupt
         ) (progDesc
@@ -65,6 +71,7 @@ main = do
         , " timeclock file for budgeting and reporting."])))
     case parsed of
         Start account comment -> startWork settings account comment
+        Status -> queryStatus settings
         Interrupt -> interrupt settings
         RemindMe minutes message -> remindMe minutes message
 
@@ -78,15 +85,34 @@ startWork settings account comment =
             (if Text.null account then _defaultAccount settings else account)
             comment)
         void (timeout (usecFromMinutes (_sessionLength settings))
-            (recvUDP settings))
+            (handleSession settings startTime))
         stopTime <- getZonedTime
         appendFile (_timeclockPath settings) (clockout stopTime)
         notifySend "Work is over!"
             ("You have worked for " <> show (minuteDiff stopTime startTime) <> " minutes. Well done!")
 
 
+handleSession :: Settings -> ZonedTime -> IO ()
+handleSession settings startTime = do
+    request <- recvUDP settings
+    case request of
+        "interrupt" -> pure ()
+        "status" -> do
+            nowTime <- getZonedTime
+            sendUDP settings (show (minuteDiff nowTime startTime))
+            handleSession settings startTime
+        _ -> handleSession settings startTime
+
+
+queryStatus :: Settings -> IO ()
+queryStatus settings = do
+    sendUDP settings "status"
+    response <- recvUDP settings
+    putStrLn ("Current work session: " <> response <> "/" <> show (_sessionLength settings) <> " minute(s)")
+    
+
 interrupt :: Settings -> IO ()
-interrupt settings = do
+interrupt settings =
     sendUDP settings "interrupt"
 
 
